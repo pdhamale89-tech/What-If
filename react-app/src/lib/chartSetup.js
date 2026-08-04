027f61d209d7dea2156d2393b35764b9e5a14e47
pdhamale89-tech <pdhamale89@gmail.com>
2026-08-04 16:09:57 +0530

    Fix blank page: register Chart.js BarController/LineController

--- react-app/src/lib/chartSetup.js
+++ react-app/src/lib/chartSetup.js
@@ -5,12 +5,14 @@ import {
   BarElement,
   LineElement,
   PointElement,
+  BarController,
+  LineController,
   Title,
   Tooltip,
   Legend,
 } from 'chart.js'
 import ChartDataLabels from 'chartjs-plugin-datalabels'
 
-ChartJS.register(CategoryScale, LinearScale, BarElement, LineElement, PointElement, Title, Tooltip, Legend, ChartDataLabels)
+ChartJS.register(CategoryScale, LinearScale, BarElement, LineElement, PointElement, BarController, LineController, Title, Tooltip, Legend, ChartDataLabels)
 
 export const endsOnly = (ctx) => ctx.dataIndex === 0 || ctx.dataIndex === ctx.dataset.data.length - 1
